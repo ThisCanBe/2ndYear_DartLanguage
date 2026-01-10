e0b710c72cfd70b27c0a35ef5ace2126135d17d0
ThisCannotBe <danielmagsakay733@gmail.com>
2026-01-10 11:15:32 +0800

    Add files via upload

---
+++ beark_labels_nested_loop.dart
@@ -0,0 +1,35 @@
+
+void main() {
+  // Break Keyword
+  // outerloop is Label
+  // for( int i = 1; i < 10; i++){
+  //   print(i);
+  //   if(i == 5){
+  //     break;
+  //   }
+  // }
+
+  // OuterLoop: for( int i = 1; i <= 3; i++){
+  //   for(int j = 1; j <= 3; j++){
+  //     print("$i $j");
+  //     if(i == 2 && j == 2){
+  //       break OuterLoop;
+  //     }
+  //   }
+  // }
+  // Continue Keyword
+  // for(int i = 1; i <= 10; i++){
+  //   if(i % 2 == 0){
+  //     continue;
+  //   }
+  //   print(i);
+  // }
+  // outerloop: for(int i = 1; i <= 3; i++){
+  //   for(int j = 1; j <= 3; j++){
+  //     if(i == 2 && j == 2){
+  //       continue outerloop;
+  //     }
+  //     print("$i $j");
+  //   }
+  // }
+}
---
+++ loop.dart
@@ -0,0 +1,83 @@
+void main() {
+  /*For Loop Sytanx
+    for (initializer; condition; increment/decrement){
+      put your code here
+    }
+
+
+    for(int i = 1; i < 5; i ++){
+      print("Hello");
+    }
+        Initializer -> condition check -> code execute    ->  increment/decrement       OUTPUT:
+  Loop 1    i = 1   ->  yes  ->  true  -> print("Hello"); ->   i++   ->  i = 2           Hello
+  Loop 2            ->  yes  ->  true  -> print("Hello"); ->   i++   ->  i = 3           Hello
+  Loop 3            ->  yes  ->  true  -> print("Hello"); ->   i++   ->  i = 4           Hello
+End of Loop         ->  No   -> False  -> Loop Terminates
+  */
+
+  /* Here's the Code down below*/
+  // for(int i = 0; i < 10; i++){
+  //  if(i % 2 == 0 ){
+  //   print(i);
+  //  }
+  // }
+  // List planetList = ["Mercury", "Venus", "Earth", "Mars"];
+  //   for(String planet in planetList){
+  //     print(planet);
+  //   }
+
+  /*While Loop sytanx
+    Initializer counter
+    while(condition){
+      code here
+      increment/decrement    
+    }
+
+    int i = 0;
+    while(i < 3){
+      print("Hello");
+      i++;
+    }
+          condition check -> code execute    ->  increment/decrement       OUTPUT:
+  Loop 1   0 < 3 -> true  -> print("Hello"); ->       i++ -> i = 1          Hello
+  Loop 2   1 < 3 -> true  -> print("Hello"); ->       i++ -> i = 2          Hello
+  Loop 3   2 < 3 -> true  -> print("Hello"); ->       i++ -> i = 3          Hello
+End of Loop  3 < 3 -> false -> Loop Terminates
+  */
+   /* Here's the Code down below*/
+  // int i = 1;
+  //   while( i <= 10){
+  //     print(i);
+  //     i++;
+  //   }
+
+/* Do while Loop syntax
+  Initializer counter
+  
+  do{
+    code here
+    increment/decrement
+  }while(condition);
+
+int i = 0;
+
+do{
+  print("Hello");
+  i++;
+}while(i < 3);
+
+          Code execution  -> Increment/Decrement   ->  Condition check       OUTPUT:
+  Loop 1  print("Hello"); ->   i++ -> i = 1        ->   1 < 3 -> true        Hello
+  Loop 2  print("Hello"); ->   i++ -> i = 2        ->   2 < 3 -> true        Hello
+  Loop 3  print("Hello"); ->   i++ -> i = 3        ->   3 < 3 -> false
+End of Loop  3 < 3 -> false -> Loop Terminates
+
+*/
+ /* Here's the Code down below*/
+  // int i = 1;
+  // do {
+  //   print(i);
+  //   i++;
+  // }
+  // while(i <= 10);
+}
