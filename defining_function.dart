ef15b75bd125f29e1e4eed384ed356a0fcdae68b
ThisCannotBe <danielmagsakay733@gmail.com>
2026-01-10 15:02:40 +0800

    Add files via upload

---
+++ defining_function.dart
@@ -0,0 +1,31 @@
+
+// void main() {
+//  findperimeter();
+// }
+// void findperimeter(){
+//   int lenght = 4;
+//   int height = 2;
+
+//   int perimeter = 2 * (lenght + height);
+
+//   print("The perimeter is $perimeter");
+// }
+
+
+// void main() {
+//  findperimeter(4, 2); // call out the perimeter value
+//  int rectarea = getarea(10,5);  
+
+//  print("The rectangle area is $rectarea"); // call out the value of rectangle area
+// }
+// void findperimeter(int lenght, int height){
+
+//   int perimeter = 2 * (lenght + height);
+
+//   print("The perimeter is $perimeter");
+// }
+
+// int getarea(int lenght, int height){
+//   int area = lenght * height; // calculate the area
+//   return area; // To return the calculate area 
+// }
